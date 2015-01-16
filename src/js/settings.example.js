be0a2148304a5a5acef0b0b8bc477a7943f7207c
Danny Koppenhagen <Danny.Koppenhagen@t-online.de>
2015-01-16 14:11:32 +0100

    updated example data for github

--- src/js/settings.example.js
+++ src/js/settings.example.js
@@ -2,5 +2,21 @@ var config = {
     wlan:  {
         name: "WLANNAME",  // "WLANNAME"
         key: "WLANKEY"    // "WLANKEY"
+    },
+    lvb: {
+        stations : [  // array with hfas-IDs
+            "13247","11563"
+        ],
+        max_stations : "10",
+        clientID : generateClientID()
     }
+}
+
+function generateClientID (){
+    var clientID ="";
+    for(var i = 0; i < 23; i++){
+        clientID += Math.floor(Math.random()*10);
+    }
+    console.log("generating new clientID...", clientID);
+    return clientID;
 }
