ee8f9e1ed4af0dd22258c56799250e82de12e6af
Danny Koppenhagen <Danny.Koppenhagen@t-online.de>
2015-01-16 15:25:30 +0100

    implemented random time refresh

--- src/js/controller/lvb.controller.js
+++ src/js/controller/lvb.controller.js
@@ -3,21 +3,32 @@
     "use strict";
 	angular
 		.module('wgscreen')
-        .controller('lvbCtrl', ['$scope', '$http', '$routeParams',
-            function($scope, $http, $routeParams) {
+        .controller('lvbCtrl', ['$scope', '$timeout', '$http', '$routeParams',
+            function($scope, $timeout, $http, $routeParams) {
                 console.log('open lvb page...');
-                //$scope.stationlimit = config.lvb.max_stations;
 
-                $http.post('php/stations.php', config.lvb)
-                    .success(insertLVBData)
-                    .error(function(data, status, headers, config) {
-                        console.log("Error by getting data", data, status, headers, config);
-                });
+                var refresh  = function(){
+                    console.log("Refreshing duration monitor...");
+                        $http.post('php/stations.php', config.lvb)
+                            .success(insertLVBData)
+                            .error(function(data, status, headers, config) {
+                            console.log("Error by getting data", data, status, headers, config);
+                        });
+                        function insertLVBData (data) {
+                            console.log(data);
+                            $scope.stations = data;
+                        }
+                    $timeout(refresh,randomTime());
+                }
+                $timeout(refresh,0);
 
+                $scope.stationlimit = config.lvb.max_stations;
 
-                function insertLVBData (data) {
-                    console.log(data);
-                    $scope.stations = data;
+                function randomTime(){
+                    /*  val1:2min (+val2)    ; val2: 4min (min)*/
+                    var random=Math.floor((Math.random()*(2000))+(4000))*60;
+                    console.log(random);
+                    return random;
                 }
         }]);
 }());
