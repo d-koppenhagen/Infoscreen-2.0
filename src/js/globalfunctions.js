7b2268dc1fd867e34a0dab863838c9b302bb08d5
Danny Koppenhagen <Danny.Koppenhagen@t-online.de>
2015-01-22 10:38:32 +0100

    refresh weather data

--- src/js/globalfunctions.js
+++ src/js/globalfunctions.js
@@ -18,6 +18,56 @@ function initWebRTC() {
         $("#ownVideoContainer").removeClass("hidden");
         //$location.path( "/call" );
     });
+
+    webrtc.on('videoAdded', function (video, peer) {
+        console.log('video added', peer);
+        var remotes = document.getElementById('remotes');
+        if (remotes) {
+            var pan = document.createElement('div');
+            pan.className = 'panel panel-primary';
+
+            var panHeading = document.createElement('div');
+            panHeading.className = 'panel-heading';
+
+            var btn = document.createElement('button');
+            btn.className = 'close';
+            btn.setAttribute ('type','button');
+            btn.setAttribute ('data-dismiss','remoteVideoPanel');
+
+            var panHeadingText = document.createElement('h3');
+            panHeadingText.className = 'panel-title';
+            panHeadingText.textContent = 'Peer "'+peer.id+'"';
+
+            panHeading.appendChild(panHeadingText);
+            panHeading.appendChild(btn);
+            pan.appendChild(panHeading)
+
+            var d = document.createElement('div');
+            d.className = 'videoContainer embed-responsive embed-responsive-4by3';
+            d.id = 'container_' + webrtc.getDomId(peer);
+            video.className = 'embed-responsive-item';
+            d.appendChild(video);
+            pan.appendChild(d);
+            var vol = document.createElement('div');
+            vol.id = 'volume_' + peer.id;
+            vol.className = 'volume_bar';
+            video.onclick = function () {
+                video.style.width = video.videoWidth + 'px';
+                video.style.height = video.videoHeight + 'px';
+            };
+            pan.appendChild(vol);
+            remotes.appendChild(pan);
+        }
+    });
+
+    webrtc.on('videoRemoved', function (video, peer) {
+        console.log('video removed ', peer);
+        var remotes = document.getElementById('remotes');
+        var el = document.getElementById('container_' + webrtc.getDomId(peer));
+        if (remotes && el) {
+            remotes.removeChild(el.parent().parent().parent());
+        }
+    });
 }
 
 function generateID() {
