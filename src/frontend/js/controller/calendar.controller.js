e98b01a9aee9ffa6b5dbc777f43a84fcfa47ed69
Danny Koppenhagen <Danny.Koppenhagen@t-online.de>
2015-05-10 15:40:52 +0200

    ...

--- src/frontend/js/controller/calendar.controller.js
+++ src/frontend/js/controller/calendar.controller.js
@@ -6,6 +6,14 @@
             function($scope, $routeParams, $http) {
                 console.log('open calendar page...');
 
-            
+                $scope.today = function() {
+                    $scope.dt = new Date();
+                  };
+                $scope.today();
+
+                $scope.clear = function () {
+                    $scope.dt = null;
+                };
+
         }]);
 }());
--- src/frontend/js/wgscreen.module.js
+++ src/frontend/js/wgscreen.module.js
@@ -1,6 +1,6 @@
 (function(){
 	'use strict';
     angular.module('wgscreen', ['ngRoute','feeds','ngSanitize','ngResource','ui.bootstrap'], function(){
-			$.material.init();
+			//$.material.init();
 		});
 }());
