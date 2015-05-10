a8a5cf1fbed561c918edab55b76c4f6cd903ef57
Danny Koppenhagen <Danny.Koppenhagen@t-online.de>
2015-05-10 22:39:01 +0200

    cleaned code

--- src/frontend/js/controller/webRTC.controller.js
+++ src/frontend/js/controller/webRTC.controller.js
@@ -4,24 +4,6 @@
 		.module('wgscreen')
         .controller('webRTCctrl', ['$scope', '$routeParams',
             function($scope, $routeParams) {
-                $('#ownVideoContainer').removeClass('hidden');
-                $(".close").click(function () {
-                    $(this).parent().parent().addClass('hidden');
-                });
-                //initWebRTC();
-
-                function gotStream(stream) {
-                    window.AudioContext = window.AudioContext || window.webkitAudioContext;
-                    var audioContext = new AudioContext();
-
-                    // Create an AudioNode from the stream
-                    var mediaStreamSource = audioContext.createMediaStreamSource(stream);
-
-                    // Connect it to destination to hear yourself
-                    // or any other node for processing!
-                    mediaStreamSource.connect(audioContext.destination);
-                }
-
-                navigator.getUserMedia({audio:true}, gotStream);
+                
             }]);
 }());
